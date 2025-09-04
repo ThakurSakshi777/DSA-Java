d8c6ac3fae4d568e235dcbbac4e2883f1e563b08
sakshi singh <thakursakshisingh8318@gmail.com>
2025-09-04 13:23:14 +0530

    Add new file StarPyramid

---
+++ shapes/StarPyramid.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class StarPyramid {
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)){
+
+            	 // input for user
+            System.out.println("enter the row number");
+            int n = sc.nextInt();
+
+                   // logic 
+            for (int i = 1; i <= n; i++) { // row
+                for (int j = 1; j <= (n-i); j++) { // col
+                    System.out.print(" " + " "); // space
+                }
+                 for (int j = 1; j <= (2*i - 1); j++) { // col
+                    System.out.print("*" + " "); // hash
+                }
+                System.out.println("");
+            }
+        }
+    }
+}
