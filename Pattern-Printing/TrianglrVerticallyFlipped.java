1cedbc3698661c7960908d4814682215c668ab08
sakshi singh <thakursakshisingh8318@gmail.com>
2025-09-04 12:27:11 +0530

    Add new file TriangleVerticallyFlipped

---
+++ Pattern-Printing/TrianglrVerticallyFlipped.java
@@ -0,0 +1,34 @@
+
+// ques 
+// print n*n TriangleReverse
+
+// if be print *
+// outpur is -
+//      *
+//     **
+//   ***
+// ****
+
+// Ans
+
+import java.util.Scanner;
+
+public class TrianglrVerticallyFlipped {
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)) {
+
+             // input for user
+            System.out.println("enter the row number");
+            int n = sc.nextInt();
+
+                // logic 
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if(i+j > n) System.out.print("*");
+                    else System.out.print(" " + " ");
+                }
+                System.out.println("");
+            }
+        }
+    }
+}
